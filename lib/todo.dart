d8551e433014e41c406d8be478f4d811e57c5fc8
RewaBhardwaj <rewa1683.be20@chitkara.edu.in>
2024-02-15 03:51:43 -0800

    first commit

---
+++ lib/todo.dart
@@ -0,0 +1,28 @@
+
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:json_annotation/json_annotation.dart';
+part 'todo.g.dart';
+
+@JsonSerializable()
+@HiveType(typeId: 01)
+class Todo {
+  @HiveField(0)
+  String? title;
+   @HiveField(1)
+  String? id;
+   @HiveField(2)
+  bool? isDone;
+   @HiveField(3)
+  int? priority;
+
+
+Todo ({
+    this.title,
+    this.id,
+    this.isDone,
+    this.priority
+  }
+);
+}
+
+
